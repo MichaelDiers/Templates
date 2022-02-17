9177a157b112015bc3921cd5383617f844b5a5ea
Michael Diers <md@sreid.de>
2022-02-17 14:59:51 +0100

    remove .net template for google cloud pub/sub

--- .net/GoogleCloud.Functions.PubSub/GoogleCloud.Functions.PubSub/Contracts/IFunctionConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GoogleCloud.Functions.PubSub.Contracts
-{
-	/// <summary>
-	///   Access the application settings.
-	/// </summary>
-	public interface IFunctionConfiguration
-	{
-	}
-}
--- .net/GoogleCloud.Functions.PubSub/GoogleCloud.Functions.PubSub/Contracts/IMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GoogleCloud.Functions.PubSub.Contracts
-{
-	/// <summary>
-	///   Describes the incoming pub/sub message.
-	/// </summary>
-	public interface IMessage
-	{
-	}
-}
